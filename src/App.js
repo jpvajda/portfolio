0926b0988d4049b876fbea9cdc2046b0c7453073
John P. Vajda <john.vajda@gmail.com>
2021-01-09 09:56:21 -0700

    moved header into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.scss';
+import React from "react";
+import {Helmet} from "react-helmet"
 
 function App() {
   return (
     <div className="App"> 
-    
-    </div>
+            <Helmet>
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+                <title>John P Vajda: Portfolio</title>
+                <link rel="canonical" href="http://localhost:3000/jpvajda/jpvajda-react.github.io" />
+                <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet"/>
+                {/* new relic script for data tracking */}
+                <script src="newrelic.js"></script>
+
+            </Helmet>
+        </div>
    
   );
 }
