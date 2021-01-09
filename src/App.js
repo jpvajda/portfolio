89064a000f854c5ab4b34f8cf1f46458c3c3414a
John P. Vajda <john.vajda@gmail.com>
2021-01-09 10:49:52 -0700

    added custom component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import './App.scss';
 import React from "react";
-import {Helmet} from "react-helmet"
+import { Helmet } from "react-helmet"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ControlledCarousel from './components/ControlledCarousel';
+
 
 function App() {
   return (
-    <div className="App"> 
-            <Helmet>
-                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-                <title>John P Vajda: Portfolio</title>
-                <link rel="canonical" href="http://localhost:3000/jpvajda/jpvajda-react.github.io" />
-                <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet"/>
-                {/* new relic script for data tracking */}
-                <script src="newrelic.js"></script>
-
-            </Helmet>
-        </div>
-   
+    <div className="App">
+      <Helmet>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>John P Vajda: Portfolio</title>
+        <link rel="canonical" href="http://localhost:3000/jpvajda/jpvajda-react.github.io" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet" />
+        {/* new relic script for data tracking */}
+        <script src="newrelic.js"></script>
+      </Helmet>
+      <ControlledCarousel.js></ControlledCarousel.js>
+      <section className="projects">
+        <h1 className="project-title">Projects</h1>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+    </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </section>
+    </div>
   );
 }
 
---
+++ src/components/ControlledCarousel.js
@@ -0,0 +1,76 @@
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Component, useState } from 'react';
+
+
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  class ControlledCarousel extends Component {
+
+    render() {
+
+      return (
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=First slide&bg=373940"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>Hello!</h3>
+              <p>Hello! I am product and engineering leader who loves to code. I have 14 years of technical
+              project and technical product management experience, and over the last 4 years been
+              investing heavily in sharping my developer / engineering skill set.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Second slide&bg=282c34"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>My Mission</h3>
+              <p>To empower makers and creators to build amazing things that
+                will change the world.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=20232a"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>More about me</h3>
+              <p>To say I love empowering developers to build cool things is an understatement.
+              Great documentation, amazing code libraries, and awesome developer support enables
+              creative people to do amazing things with your software. I love seeing a
+              community grow and use a product to solve their own developer challenges.</p>
+
+              <p>I enjoy talking to customers and users about products and seeing their eyes and brains
+              light up when a particular solution solves a challenge they are facing.
+                Being in service to people, and making their lives easier, brings me joy.</p>
+
+              <p>I believe in open source, accessibility, transparency in code, and clarity in design and
+              documentation. I possess an insatiable appetite to learn, and ability to
+                learn quickly and love for coding that goes beyond words.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      );
+    }
+  }
+}
+
+export default ControlledCarousel;
+
+
