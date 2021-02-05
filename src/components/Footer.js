1ae45c87a51991ea2bcdc939efe5ec6101a67056
John P. Vajda <john.vajda@gmail.com>
2021-02-05 08:05:00 -0700

    css updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <div className="footer">
-    <p>This is some content in sticky footer</p>
+    <p>This is some content in a sticky footer</p>
   </div>
 );
 
